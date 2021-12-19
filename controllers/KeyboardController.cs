cd817e9554fa422847958c648b36c18902963095
Christopher Nguyen <Chris_ngu@pm.me>
2021-12-18 19:46:03 -0600

    adding get endpoint

--- controllers/KeyboardController.cs
+++ controllers/KeyboardController.cs
@@ -6,17 +6,30 @@ using System.Threading.Tasks;
 
 namespace KeebClack.API.controllers
 {
+    [Route("api/keyboard")]
     [ApiController]
     public class KeyboardController : ControllerBase
     {
-        [HttpGet("api/keyboards")]
-        public JsonResult GetKeyboards()
+        [HttpGet]
+        public JsonResult GetAllKeyboards()
         {
             return new JsonResult(
                 new List<object>()
                 {
                     new {id = 1, Name= "Ducky"},
                     new {id = 2, Name= "Vortex"}
+                });
+        }
+
+        [HttpGet("{id:int}")]
+        public JsonResult GetOneKeyboard(int id)
+        {
+            return new JsonResult(
+                new List<object>()
+                {
+                    new {id= 0, Name=id},
+                    new {id= 1, Name= 1},
+                    new {id= 2, Name= 2}
                 });
         }
     }
