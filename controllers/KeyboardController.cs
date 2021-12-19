96dcc5d9f88bb1ab608ed5a551d35822875f0de0
Christopher Nguyen <Chris_ngu@pm.me>
2021-12-18 19:28:48 -0600

    first commit

---
+++ controllers/KeyboardController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KeebClack.API.controllers
+{
+    [ApiController]
+    public class KeyboardController : ControllerBase
+    {
+        [HttpGet("api/keyboards")]
+        public JsonResult GetKeyboards()
+        {
+            return new JsonResult(
+                new List<object>()
+                {
+                    new {id = 1, Name= "Ducky"},
+                    new {id = 2, Name= "Vortex"}
+                });
+        }
+    }
+}
